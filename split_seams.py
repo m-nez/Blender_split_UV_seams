99c3590670510374ffa42ed04be4b033f3ef280b
m-nez <mn@openmailbox.org>
2016-01-21 13:59:44 +0100

    Split UVs on all selected objects

--- split_seams.py
+++ split_seams.py
@@ -1,20 +1,20 @@
 import bpy
 
-obj = bpy.context.active_object
+objects = bpy.context.selected_objects
+active_obj = bpy.context.active_object
 
 bpy.ops.object.mode_set(mode='OBJECT')
 
-prev_state = []
-for e in obj.data.edges:
-    prev_state.append(e.use_edge_sharp)
-    if e.use_seam:
-        e.use_edge_sharp = True
+for obj in objects:
+    for e in obj.data.edges:
+        if e.use_seam:
+            e.use_edge_sharp = True
 
-bpy.ops.object.modifier_add(type='EDGE_SPLIT')
-obj.modifiers[-1].use_edge_angle = False
-obj.modifiers[-1].use_edge_sharp = True
+    bpy.context.scene.objects.active = obj
+    bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+    obj.modifiers[-1].use_edge_angle = False
+    obj.modifiers[-1].use_edge_sharp = True
 
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier=obj.modifiers[-1].name)
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier=obj.modifiers[-1].name)
 
-for p, e in zip(prev_state, obj.data.edges):
-    e.use_edge_sharp = p
+bpy.context.scene.objects.active = active_obj
